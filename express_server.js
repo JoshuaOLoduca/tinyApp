7e411a0aa11bb135844c43ba2a6904caf931135e
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-01-24 23:58:42 +0000

    Added delete button for urls

--- express_server.js
+++ express_server.js
@@ -20,83 +20,99 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // needs to be before /:shortURL
-app.get(routes.urls + '/new', (req, res) => {
-  res.render('urls_new');
-});
 
-app.post(routes.urls, (req, res) => {
-  let longURL = req.body.longURL;
-  const id = generateRandomString();
-  
-  if (!longURL.includes('://')) {
-    longURL = 'http://' + longURL;
-  }
-  
-  urlDatabase[id] = longURL;
-  
-  res.statusCode = 302;
-  res.redirect(`${routes.urls}/${id}`);
-  // urlDatabase[Math.floor(Math.random()* 10000)] = '';
-});
-
-app.get(routes.urls, (req, res) => {
-  const templateVars = {urls: urlDatabase};
-
-  res.render('urls_index', templateVars);
-});
-
-app.get("/:shortURL", (req, res) => {
-  const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL];
-  
-  if (longURL) {
-    res.statusCode = 302;
-    res.redirect(longURL);
-  } else {
-    const templateVars = {
-      redirect: routes.urls,
-      redirectText: 'List of Your Urls',
-      display: 'No Shortened Url found',
-    };
-    notFoundRedirect(res, templateVars);
-  }
-
-});
-
-app.get(routes.urls + "/:shortURL", (req, res) => {
-
-  const id = req.params.shortURL;
-
-  const templateVars = {
-    shortURL: id,
-    longURL: urlDatabase[id],
-  };
-
-  if (templateVars.longURL) {
-    res.render('url_show',templateVars);
-  } else {
-    const templateVars = {
-      redirect: routes.urls,
-      redirectText: 'List of Valid Urls',
-      display: 'No Url found',
-    };
-    notFoundRedirect(res, templateVars);
-  }
-});
-
-app.get(routes.main, (req, res) => {
-  res.send("Hello!");
-});
-
-
-app.get(routes.urlsDbg, (req, res) => {
-  res.json(urlDatabase);
-});
+appPosts();
+appGets();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+function appPosts() {
+  app.post(`${routes.urls}/:shortURL/delete`, (req, res) => {
+    let shortURL = req.params.shortURL;
+
+    delete urlDatabase[shortURL];
+
+    res.redirect(routes.urls);
+  });
+
+  app.post(routes.urls, (req, res) => {
+    let longURL = req.body.longURL;
+    const id = generateRandomString();
+    
+    if (!longURL.includes('://')) {
+      longURL = 'http://' + longURL;
+    }
+    
+    urlDatabase[id] = longURL;
+    
+    res.statusCode = 302;
+    res.redirect(`${routes.urls}/${id}`);
+    // urlDatabase[Math.floor(Math.random()* 10000)] = '';
+  });
+}
+
+function appGets() {
+  app.get(routes.urls + '/new', (req, res) => {
+    res.render('urls_new');
+  });
+
+  app.get(routes.urls, (req, res) => {
+    const templateVars = {urls: urlDatabase};
+  
+    res.render('urls_index', templateVars);
+  });
+  
+  app.get("/:shortURL", (req, res) => {
+    const shortURL = req.params.shortURL;
+    const longURL = urlDatabase[shortURL];
+    
+    if (longURL) {
+      res.statusCode = 302;
+      res.redirect(longURL);
+    } else {
+      const templateVars = {
+        redirect: routes.urls,
+        redirectText: 'List of Your Urls',
+        display: 'No Shortened Url found',
+      };
+      notFoundRedirect(res, templateVars);
+    }
+  
+  });
+  
+  app.get(routes.urls + "/:shortURL", (req, res) => {
+  
+    const id = req.params.shortURL;
+  
+    const templateVars = {
+      shortURL: id,
+      longURL: urlDatabase[id],
+    };
+  
+    if (templateVars.longURL) {
+      res.render('url_show',templateVars);
+    } else {
+      const templateVars = {
+        redirect: routes.urls,
+        redirectText: 'List of Valid Urls',
+        display: 'No Url found',
+      };
+      notFoundRedirect(res, templateVars);
+    }
+  });
+  
+  app.get(routes.main, (req, res) => {
+    res.send("Hello!");
+  });
+  
+  
+  app.get(routes.urlsDbg, (req, res) => {
+    res.json(urlDatabase);
+  });
+
+}
 function generateRandomString(length = 6) {
   const chars = 'abcdefgh1jklmnopqrstuvwxyz'.split('');
   const numbers = '0123456789'.split('');
@@ -123,6 +139,7 @@ function generateRandomString(length = 6) {
   return randomString;
 
 }
+
 
 function notFoundRedirect(resp, templateVars, page = '404_url') {
   resp.statusCode = 404;
